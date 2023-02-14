e4c4a4fa7b4085e062b9ebc90829f6a3620843d6
berkg <berkguzeyer@yahoo.com>
2023-02-13 22:36:08 -0500

    New classes are added.

---
+++ src/test/java/technostudyB7/day5/ExplicitlyWait.java
@@ -0,0 +1,27 @@
+package technostudyB7.day5;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ExplicitlyWait {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://the-internet.herokuapp.com/dynamic_loading/1");
+
+        WebElement startButton = driver.findElement(By.cssSelector("#start>button"));
+        startButton.click();
+
+        WebDriverWait wait = new WebDriverWait(driver,10);
+
+        WebElement finishText = driver.findElement(By.id("finish"));
+        wait.until(ExpectedConditions.visibilityOf(finishText));
+    }
+}
