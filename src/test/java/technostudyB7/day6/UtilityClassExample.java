06210b9d8a94c6ad0ac94974f435ed0451856b45
berkg <berkguzeyer@yahoo.com>
2023-02-14 22:04:42 -0500

    New classes are added.

---
+++ src/test/java/technostudyB7/day6/UtilityClassExample.java
@@ -0,0 +1,9 @@
+package technostudyB7.day6;
+
+import technostudyB7.Utilities.UtilityClass;
+
+public class UtilityClassExample extends UtilityClass {
+    public static void main(String[] args) {
+        driver.get("https://www.google.com/");
+    }
+}
