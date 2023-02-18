7f46f79e8f8abbfdc89c8353e23176b3e368ccf1
berkg <berkguzeyer@yahoo.com>
2023-02-17 19:03:17 -0500

    Day 7 Classes added.

---
+++ src/test/java/technostudyB7/day7/ActionsClick.java
@@ -0,0 +1,38 @@
+package technostudyB7.day7;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import technostudyB7.Utilities.UtilityClass;
+
+public class ActionsClick extends UtilityClass {
+    public static void main(String[] args) {
+        driver.get("https://demoqa.com/buttons/");
+
+        WebElement doubleClickButton = driver.findElement(By.id("doubleClickBtn"));
+        Actions actions = new Actions(driver);
+        Action action = actions.doubleClick(doubleClickButton).build();
+        action.perform();
+
+        WebElement rightClickButton = driver.findElement(By.id("rightClickBtn"));
+        Action rightClickAction = actions.contextClick(rightClickButton).build();
+        rightClickAction.perform();
+
+        //<button id="MCxMx" type="button" class="btn btn-primary">Click Me</button>
+        WebElement clickMeButton = driver.findElement(By.xpath("//button[text()='Click Me']"));
+        Action clickAction = actions.click(clickMeButton).build();
+        clickAction.perform();
+
+        //`MOVETO` ELEMENT  -  doubleClick
+        Action doubleclickAction2 = actions.moveToElement(doubleClickButton).doubleClick().build();
+        doubleclickAction2.perform();
+
+        //MOVETO rightClick
+        Action rightClickAction2 = actions.moveToElement(rightClickButton).contextClick().build();
+        rightClickAction2.perform();
+
+
+
+    }
+}
---
+++ src/test/java/technostudyB7/day7/MentoringProject.java
@@ -0,0 +1,55 @@
+package technostudyB7.day7;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import technostudyB7.Utilities.UtilityClass;
+
+public class MentoringProject {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+
+        driver.manage().window().maximize();
+        driver.get("https://demo.guru99.com/insurance/v1/index.php");
+
+        //<a href="register.php" class="btn btn-default">Register</a>
+        WebElement registerButton = driver.findElement(By.linkText("Register"));
+        registerButton.click();
+
+        WebElement titleDropdown = driver.findElement(By.id("user_title"));
+        Select titleDropdownMenu = new Select(titleDropdown);
+        titleDropdownMenu.selectByVisibleText("Captain");
+
+        WebElement firstName = driver.findElement(By.id("user_firstname"));
+        firstName.sendKeys("John");
+
+        WebElement surname = driver.findElement(By.id("user_surname"));
+        surname.sendKeys("Doe");
+
+        WebElement phone = driver.findElement(By.id("user_phone"));
+        phone.sendKeys("123456789");
+
+        WebElement year = driver.findElement(By.id("user_dateofbirth_1i"));
+        Select selectYear = new Select(year);
+        selectYear.selectByVisibleText("1994");
+
+        WebElement month = driver.findElement(By.id("user_dateofbirth_2i"));
+        Select selectMonth = new Select(month);
+        selectMonth.selectByVisibleText("July");
+
+        WebElement day = driver.findElement(By.id("user_dateofbirth_3i"));
+        Select selectDay = new Select(day);
+        selectDay.selectByVisibleText("15");
+
+
+
+    }
+}
