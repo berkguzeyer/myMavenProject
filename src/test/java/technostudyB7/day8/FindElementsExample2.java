8e75e256e3338417b879dddafa85d8f6966ce2f8
berkg <berkguzeyer@yahoo.com>
2023-02-20 19:37:12 -0500

    Example2 has been corrected.

--- src/test/java/technostudyB7/day8/FindElementsExample2.java
+++ src/test/java/technostudyB7/day8/FindElementsExample2.java
@@ -8,13 +8,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-/**1- Go to https://www.saucedemo.com/
- 2- login with valid credentials
- 3- Add 3 random products to the cart
- 4- Click on the cart
- 5- Click on the CheckOut button
- 6- Fill the form and click on Continue
- 7- Check if you see your products*/
+/**
+ * 1- Go to https://www.saucedemo.com/
+ * 2- login with valid credentials
+ * 3- Add 3 random products to the cart
+ * 4- Click on the cart
+ * 5- Click on the CheckOut button
+ * 6- Fill the form and click on Continue
+ * 7- Check if you see your products
+ */
 
 public class FindElementsExample2 extends UtilityClass {
     public static void main(String[] args) {
@@ -30,26 +32,53 @@ public class FindElementsExample2 extends UtilityClass {
         WebElement loginButton = driver.findElement(By.xpath("//input[@data-test='login-button']"));
         loginButton.click();
 
-        List<WebElement> productList = driver.findElements(By.xpath("//div[@class='inventory_item_name']"));
-        List<String> inventoryItemNames = new ArrayList<>();
+        List<WebElement> inventoryItems;
+        List<String> inventoryItemsNames = new ArrayList<>();
         List<Integer> indexList = new ArrayList<>();
 
+        //TODO: complete and explain this part
+        while (inventoryItemsNames.size() < 3) {
+            inventoryItems = driver.findElements(By.cssSelector(".inventory_item_name"));
+            int randomIndex = (int) (Math.random() * inventoryItems.size());
 
-        while(productList.size()<3) {
-            int randomNumber = (int) (Math.random() * productList.size());
-            if (indexList.contains(randomNumber)){
+            if (indexList.contains(randomIndex)) {
                 continue;
             }
-            indexList.add(randomNumber);
-            inventoryItemNames.add(productList.get(randomNumber).getText());
-            productList.get(randomNumber).click();
+            indexList.add(randomIndex);
+            inventoryItemsNames.add(inventoryItems.get(randomIndex).getText());
+            inventoryItems.get(randomIndex).click();
             WebElement addToCartButton = driver.findElement(By.xpath("//button[text()='Add to cart']"));
             addToCartButton.click();
             driver.navigate().back();
         }
 
-        WebElement cart = driver.findElement(By.xpath("//a[@class='shopping_cart_link']"));
-        cart.click();
+        WebElement cartIcon = driver.findElement(By.className("shopping_cart_badge"));
+        cartIcon.click();
+
+        WebElement checkOutButton = driver.findElement(By.id("checkout"));
+        checkOutButton.click();
+
+        WebElement nameInput = driver.findElement(By.id("first-name"));
+        nameInput.sendKeys("John");
+
+        WebElement lastNameInput = driver.findElement(By.id("last-name"));
+        lastNameInput.sendKeys("Snow");
+
+        WebElement zipCode = driver.findElement(By.id("postal-code"));
+        zipCode.sendKeys("44444");
+
+
+        WebElement continueButton = driver.findElement(By.id("continue"));
+        continueButton.click();
+
+        List<WebElement> checkOutItemsList = driver.findElements(By.className("inventory_item_name"));
+
+        for (WebElement product : checkOutItemsList) {
+            System.out.println(inventoryItemsNames.contains(product.getText()));
+
+        }
+
+        quitDriver(2);
 
 
     }
